21e74ab5661f65eaf3991a5c224c178e40bcc2c9
Dung Pham <112083519+dungdpham@users.noreply.github.com>
2024-02-27 11:58:53 +0200

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Checking to see if everything is still ok..." );
         System.out.println( "Goodbye World!" );
     }
 }
